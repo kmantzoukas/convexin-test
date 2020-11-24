4e5fd9456a053f05552f3a767f984bd69de91057
kmantzoukas <kmantzoukas.uk@gmail.com>
2020-11-24 22:34:09 +0200

    Added an other implementation where possible exception are being taken care of with the usage of the Scala build-in Try[A] monand

--- src/main/scala/ConvexInTestAssignment.scala
+++ src/main/scala/ConvexInTestAssignment.scala
@@ -1,27 +1,75 @@
 import ConvexInUtilities.{aggregateValuesForKeys, prepareAndCleanUpData, readDataFromS3, writeDataToS3}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+
+import scala.util.{Failure, Success, Try}
 
 object ConvexInTestAssignment {
 
   def main(args: Array[String]): Unit = {
     args match {
       case Array(master, ipath, opath) => executePipeline(master, ipath, opath)
-      case _  => println("The list of parameters is not correct.")
+      case _ => println("The list of parameters is not correct.")
     }
   }
 
-  def executePipeline(master:String, ipath:String, opath:String) = {
-    implicit val spark = SparkSession.builder.master(master).getOrCreate
+  def executePipeline(master: String, ipath: String, opath: String): Unit = {
 
-    // requires the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables to be set
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
+    implicit val spark: SparkSession = SparkSession.builder.master(master).getOrCreate
+    spark.sparkContext.hadoopConfiguration.set("fs.s3a.aws.credentials.provider",
+      "com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
 
-    val data = readDataFromS3(ipath)
-    val preparedAndClearedUpData = prepareAndCleanUpData(data)
-    val aggregatedData = aggregateValuesForKeys(preparedAndClearedUpData)
-    writeDataToS3(aggregatedData, opath)
+    /*
+    readDataFromS3(ipath) match {
+      case Success(data) => {
+        val preparedAndClearedUpData = prepareAndCleanUpData(data)
+        preparedAndClearedUpData match {
+          case Success(data) => {
+            val aggregatedData = aggregateValuesForKeys(data)
+            aggregatedData match {
+              case Success(data) => {
+                writeDataToS3(data, opath) match {
+                  case Success(_) => println("Results file has been created successfully.")
+                  case Failure(e) => println(s"An error occurred while saving the result file in $opath. Error: ${e.getMessage}")
+                }
+              }
+              case Failure(e) => println(s"An error occurred while aggregating the data. Error: ${e.getMessage}")
+            }
+          }
+          case Failure(e) => println(s"An error occurred while cleaning up the data. Error: ${e.getMessage}")
+        }
+      }
+      case Failure(e) => println(s"Unable to load file(s) from path $ipath with error: ${e.getMessage}")
+    }
+    */
+
+
+
+    /*
+    readDataFromS3(ipath).flatMap{
+      dataLoaded =>
+        prepareAndCleanUpData(dataLoaded).flatMap{
+          preparedAndClearedUpData =>
+            aggregateValuesForKeys(preparedAndClearedUpData).flatMap{
+              aggregatedData => writeDataToS3(aggregatedData, opath)
+            }
+        }
+    }
+    */
+
+    val res: Try[Unit] =
+      for {
+        loadedData <- readDataFromS3(ipath)
+        cleanedUpData <- prepareAndCleanUpData(loadedData)
+        aggregatedData <- aggregateValuesForKeys(cleanedUpData)
+        result <- writeDataToS3(aggregatedData, opath)
+      } yield result
+
+    res match {
+      case Success(_) => println("Result tsv file has been successfully created")
+      case Failure(e) => println(s"Result tsv file has not been created with error: ${e.getMessage}")
+    }
 
     spark.stop
   }
-
 }
--- src/main/scala/ConvexInUtilities.scala
+++ src/main/scala/ConvexInUtilities.scala
@@ -1,38 +1,55 @@
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
+import scala.util.{Failure, Success, Try}
+
 object ConvexInUtilities {
 
-  private def zeroIfEmpty(s:String):Int = if(s.trim.isEmpty) 0 else s.toInt
+  private def zeroIfEmpty(s: String): Int = if (s.trim.isEmpty) 0 else s.toInt
 
   private def areAllDigits(x: String) = x forall Character.isDigit
 
-def readDataFromS3(inputPath: String)(implicit spark: SparkSession) =
-  spark.sparkContext.textFile(s"$inputPath/*.*sv") // load all files with suffix *.*sv e.g. file-1.csv, file-2.tsv, etc.
+  def readDataFromS3(inputPath: String)(implicit spark: SparkSession): Try[RDD[String]] = {
+    try {
+      Success(spark.sparkContext.textFile(s"$inputPath/*.*sv"))
+    } catch {
+      case e: Throwable => Failure(e)
+    }
+  }
 
-def prepareAndCleanUpData(data: RDD[String]) =
-  data.
-    map(record => {
-      val key = record.split(",|\t+")(0)
-      val value = record.split(",|\t+")(1)
-      (key, value)
-    }).
-    filter{ case (key, value) => areAllDigits(key) && areAllDigits(value)}. // filter out the headers
-    map{case (key, value) => ((zeroIfEmpty(key), zeroIfEmpty(value)), 1)} // if the keys/values are empty we will use zero
+  def prepareAndCleanUpData(data: RDD[String]) = {
+    try {
+      Success(data.map(record => {
+        val key = record.split(",|\t+")(0)
+        val value = record.split(",|\t+")(1)
+        (key, value)
+      }).
+        filter { case (key, value) => areAllDigits(key) && areAllDigits(value) }.
+        map { case (key, value) => ((zeroIfEmpty(key), zeroIfEmpty(value)), 1) })
+    } catch {
+      case e: Throwable => Failure(e)
+    }
+  }
 
-def aggregateValuesForKeys(data: RDD[((Int, Int), Int)]) =
-  data.
-    reduceByKey(_ + _).
-    filter{case (key, counter) => (counter % 2) != 0}.
-    map{case (key, _) => key} // dismiss the number of occurrences
+  def aggregateValuesForKeys(data: RDD[((Int, Int), Int)]): Try[RDD[(Int, Int)]] = {
+     try{
+       Success(data.reduceByKey(_ + _).filter { case (key, counter) => (counter % 2) != 0 }.map { case (key, _) => key })
+     } catch {
+       case e: Throwable => Failure(e)
+     }
+  }
 
-def writeDataToS3(data: RDD[(Int, Int)], outputFile: String)(implicit spark: SparkSession) =
-  spark.
-    createDataFrame(data).
-    coalesce(1). // write the results on a single file for convenience - might not be appropriate on production
-    write.option("header", "false").
-    option("delimiter", "\t").
-    mode("overwrite").
-    csv(outputFile)
-
+  def writeDataToS3(data: RDD[(Int, Int)], outputFile: String)(implicit spark: SparkSession): Try[Unit] = {
+    try {
+      Try(spark.
+        createDataFrame(data).
+        coalesce(1). // write the results on a single file for convenience - might not be appropriate on production
+        write.option("header", "false").
+        option("delimiter", "\t").
+        mode("overwrite").
+        csv(outputFile))
+    } catch {
+      case e: Throwable => Failure(e)
+    }
+  }
 }
